9295e880a4acaff95c18a67c6005f430e4ada89d
yurividal <yurividal@gmail.com>
2016-05-17 22:04:09 -0300

    Gokyu Funcionando

--- app/src/main/java/br/pl/iJudo/ui/fragments/Gokyo.java
+++ app/src/main/java/br/pl/iJudo/ui/fragments/Gokyo.java
@@ -1,5 +1,6 @@
 package br.pl.iJudo.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,7 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ListView;
 import android.widget.SimpleExpandableListAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,11 +18,15 @@ import java.util.List;
 import java.util.Map;
 
 import br.pl.iJudo.R;
+import br.pl.iJudo.ui.golpe;
+import butterknife.ButterKnife;
 
 public class Gokyo extends Fragment {
+
     private ExpandableListAdapter mAdapter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,                   Bundle              savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_gokyo, null);
 
         return v;
@@ -28,60 +35,136 @@ public class Gokyo extends Fragment {
     private static final String NAME = "NAME";
     private static final String IS_EVEN = "IS_EVEN";
 
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
         List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
-
         for (int i = 0; i < 5; i++) {
             Map<String, String> curGroupMap = new HashMap<String, String>();
-
             groupData.add(curGroupMap);
-
-            switch (i){
-                case 0: curGroupMap.put(NAME, "Primeiro Gokyo");
-                    curGroupMap.put(IS_EVEN, "Teste");
-                    break;
-                case 1: curGroupMap.put(NAME, "Segundo Gokyo");
-                    curGroupMap.put(IS_EVEN, "Teste");
-                    break;
-                case 2: curGroupMap.put(NAME, "Terceiro Gokyo");
-                    curGroupMap.put(IS_EVEN, "Teste");
-                    break;
-                case 3: curGroupMap.put(NAME, "Quarto Gokyo");
-                    curGroupMap.put(IS_EVEN, "Teste");
-                    break;
-                case 4: curGroupMap.put(NAME, "Quinto Gokyo");
-                    curGroupMap.put(IS_EVEN, "Teste");
-                    break;
-            }
+            curGroupMap.put(NAME, Gokyu[i][0] );
+            curGroupMap.put(IS_EVEN, Gokyu[i][1] );
 
             List<Map<String, String>> children = new ArrayList<Map<String, String>>();
-            for (int j = 0; j < 2; j++) {
+            for (int j = 0; j < 8; j++) {
                 Map<String, String> curChildMap = new HashMap<String, String>();
                 children.add(curChildMap);
-                curChildMap.put(NAME, "nome do golpe");
-                curChildMap.put(IS_EVEN, "  ");
+                curChildMap.put(NAME, golpesgokyu[i][j]);
+                curChildMap.put(IS_EVEN, " ");
             }
             childData.add(children);
         }
 
+
         ExpandableListView lv = (ExpandableListView) getActivity().findViewById(R.id.expandableListView);
+
+        ExpandableListView.OnChildClickListener occl = new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                String textonaposicao;
+                Intent meuGolpe;
+                textonaposicao = golpesgokyu[groupPosition][childPosition];
+                //Toast.makeText(getActivity().getApplicationContext(), textonaposicao, Toast.LENGTH_SHORT).show();
+
+                String nomelimpo;
+                nomelimpo = textonaposicao.toLowerCase().replace(" ", "").replace("-", "");
+
+
+                meuGolpe = new Intent(getActivity().getApplicationContext(), golpe.class);
+                meuGolpe.putExtra("xmlfile", nomelimpo);
+                meuGolpe.putExtra("nomedogolpe", textonaposicao);
+
+                startActivity(meuGolpe);
+
+                return false;
+            }
+        };
+
+        lv.setOnChildClickListener(occl);
+
         // Set up our adapter
+
         mAdapter = new SimpleExpandableListAdapter(
                 getActivity(),
                 groupData,
-                android.R.layout.simple_expandable_list_item_1,
+                android.R.layout.simple_expandable_list_item_2,
                 new String[] { NAME, IS_EVEN },
                 new int[] { android.R.id.text1, android.R.id.text2 },
                 childData,
-                android.R.layout.simple_expandable_list_item_2,
+                android.R.layout.simple_expandable_list_item_1,
                 new String[] { NAME, IS_EVEN },
                 new int[] { android.R.id.text1, android.R.id.text2 }
         );
         lv.setAdapter(mAdapter);
+
+    }
+
+    private String [][] Gokyu = { {"DAI-IKKYO", "Primeiro Gokyu"} , {"DAI-NIKYO", "Segundo Gokyo"} , {"DAI-SANKIO", "Terceiro Gokyo"}, {"DAI-YONKYO", "Quarto Gokyo"} , {"DAI-GOKYO", "Quinto Gokyo"} };
+    private String golpesgokyu [][] = {
+            {"De-Ashi-Barai",
+                    "Hiza-Guruma",
+                    "Sassae-Tsuri-Komi-Ashi",
+                    "Uki-Goshi",
+                    "O-soto-Gari",
+                    "O-Goshi",
+                    "Ouchi-Gari",
+                    "Ippon-Seoi-Nage"
+            },
+            {
+                    "Ko-Soto-Gari",
+                    "Ko-Uchi-Gari",
+                    "Koshi-Guruma",
+                    "Tsuri-Komi-Goshi",
+                    "Okuri-Ashi-Barai",
+                    "Tai-Otoshi",
+                    "Harai-Goshi",
+                    "Uchi-Mata"
+
+            },
+            {
+                    "Ko-Soto-Gake",
+                    "Tsuri-Goshi",
+                    "Yoko-Otoshi",
+                    "Ashi-Guruma",
+                    "Hane-Goshi",
+                    "Harai-Tsuri-Komi-Ashi",
+                    "Tomoe-Nage",
+                    "Kata-Guruma"
+
+            },
+            {
+                    "Sumi-Gaeshi",
+                    "Tani-Otoshi",
+                    "Hane-Makikomi",
+                    "Sukui-Nage",
+                    "Utsuri-Goshi",
+                    "O-Guruma",
+                    "Soto-Makikomi",
+                    "Uki-Otoshi"
+            },
+            {
+                    "O-Soto-Guruma",
+                    "Uki-Waza",
+                    "Yoko-Wakare",
+                    "Yoko-Guruma",
+                    "Ushiro-Goshi",
+                    "Ura-Nage",
+                    "Sumi-Otoshi",
+                    "Yoko-Gake"
+            }
+    };
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
 
